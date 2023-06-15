6be54a20f0cd016d3631521e048616b09f1252e0
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-06-14 20:49:22 -0700

    bad at ACPing so this is everything

---
+++ tests/test_madlib.py
@@ -0,0 +1,48 @@
+import pytest
+# from madlib_cli.madlib import read_template, parse_template, merge
+from ..madlib_game.madlib import whole_word_list
+
+def test_read_template_returns_stripped_string():
+    actual = read_template("assets/dark_and_stormy_night_template.txt")
+    expected = "It was a {Adjective} and {Adjective} {Noun}."
+    assert actual == expected
+
+
+@pytest.mark.skip("pending")
+def test_parse_template():
+    actual_stripped, actual_parts = parse_template(
+        "It was a {Adjective} and {Adjective} {Noun}."
+    )
+    expected_stripped = "It was a {} and {} {}."
+    expected_parts = ("Adjective", "Adjective", "Noun")
+
+    assert actual_stripped == expected_stripped
+    assert actual_parts == expected_parts
+
+
+@pytest.mark.skip("pending")
+def test_merge():
+    actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
+    expected = "It was a dark and stormy night."
+    assert actual == expected
+
+
+@pytest.mark.skip("pending")
+def test_read_template_raises_exception_with_bad_path():
+
+    with pytest.raises(FileNotFoundError):
+        path = "missing.txt"
+        read_template(path)
+
+
+def test_first_word():
+    actual = whole_word_list[0]
+    expected = 'Adjective'
+    assert actual == expected
+
+
+def test_third_word():
+    actual = whole_word_list(2)
+    expected = 'A First Name'
+    assert actual == expected
+
