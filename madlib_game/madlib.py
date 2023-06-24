b0da7cd774dbfa3cfceb9db983cd3e1193248e78
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-06-24 12:39:43 -0700

    lab03 edits for resubmit

--- madlib_game/madlib.py
+++ madlib_game/madlib.py
@@ -1,17 +1,17 @@
 import re
-from tests.test_madlib import *
-whole_word_list = ["Adjective"]
 
-def main():
   ## print welcome message explaining process
-  print( """
+print( """
   Welcome to MadLibs! Make up your own words to fill the prompts correctly to create a fun story. """)
 
 
   ## open and read template, turn into a string
-  with open('madlib_template.txt', 'r') as file:
+def read_template(file_path):
+  with open(file_path, 'r') as file:
     template = file.read()  ## template is a string 
+    return template
 
+def parse_template(template):
   non_curly = False
   story_dict = ''
   story_list = []
@@ -39,35 +39,47 @@ def main():
       else:
         if char not in ignore:
           empty_filler += char
+  return story_dict, tuple(whole_word_list)
       
   # print(story_list)
   # print(whole_word_list)
 
   ## Prompt user to submit words for required components  
   ## Take user input and populate template with the answers
-  for i in whole_word_list:
-    user_input = input(f"Enter any word that is a(n) {i}")
-    temp = new_list.append(user_input)
 
-  print(new_list)
-
+def the_new_list(whole_word_list):
+    new_whole_word_list = []
+  
+    for i in whole_word_list:
+      user_input = input(f"Enter any word that is a(n) {i}")
+      new_whole_word_list.append(user_input)
+    print(new_whole_word_list)
+    return tuple(new_whole_word_list)
 
   ## Write completed text to a new file 
 
   # for word in new_list:
+
+def merge(story_dict, new_whole_word_tuple):
+  new_whole_word_list = list(new_whole_word_tuple)
   pattern = r"\{([^}]*)\}"
-  result = re.sub(pattern, lambda word: new_list.pop(0), template) # lambda is a mini function, iterating through new word list each time, then popping the 0 at index
-  # print(result)
-
-
-  with open('finished_madlib.txt', 'w') as file:
-    template = file.write(result)
-
-  ## Provide the completed response back to user in command line
+  result = re.sub(pattern, lambda word: new_whole_word_list.pop(0), story_dict) # lambda is a mini function, iterating through new word list each time, then popping the 0 at index
+    ## Provide the completed response back to user in command line
   print(result)
+  return result
+
+def write_to_file(file_path, mad_lib):
+  with open(file_path, 'w') as file:
+    file.write(mad_lib)
+
+
 
   ## Add at least one more "meaningful" test (not input/output) to the test file
 
 if __name__ == '__main__':
-  main()
-
+  template = read_template('madlib.txt')
+  stripped_template, parts = parse_template(template)
+  the_new_list = the_new_list(parts)
+  final_story = merge(stripped_template, the_new_list)
+  write_to_file('finished_madlib.txt', final_story)
+  parse_template(template)
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,6 +1,6 @@
 import pytest
-# from madlib_cli.madlib import read_template, parse_template, merge
 from ..madlib_game.madlib import whole_word_list
+from ..madlib_game.madlib import read_template, parse_template, merge
 
 def test_read_template_returns_stripped_string():
     actual = read_template("assets/dark_and_stormy_night_template.txt")
@@ -8,7 +8,7 @@ def test_read_template_returns_stripped_string():
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_parse_template():
     actual_stripped, actual_parts = parse_template(
         "It was a {Adjective} and {Adjective} {Noun}."
@@ -20,14 +20,14 @@ def test_parse_template():
     assert actual_parts == expected_parts
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_merge():
     actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
     expected = "It was a dark and stormy night."
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_read_template_raises_exception_with_bad_path():
 
     with pytest.raises(FileNotFoundError):
@@ -35,13 +35,13 @@ def test_read_template_raises_exception_with_bad_path():
         read_template(path)
 
 
-def test_first_word():
+# def test_first_word():
     actual = whole_word_list[0]
     expected = 'Adjective'
     assert actual == expected
 
 
-def test_third_word():
+# def test_third_word():
     actual = whole_word_list(2)
     expected = 'A First Name'
     assert actual == expected
